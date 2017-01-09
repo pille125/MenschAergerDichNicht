596b1f29198a462904f3cbd1fa85da68adcb329e
pille125 <pille125@hotmail.de>
2017-01-09 14:40:40 +0100

    added Playfield class

---
+++ MenschAergerDichNicht/src/com/company/Playfield.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Vector;
+
+/**
+ * Created by pille125 on 09.01.17.
+ */
+public class Playfield {
+    private static Playfield instance = null;
+    private Vector<Piece> fields;
+
+    //Constructor
+    private Playfield() {
+        this.fields = new Vector<Piece>(56);
+        this.fields.setSize(56);
+    }
+
+    //Singelton
+    public static Playfield getInstance() {
+        if (instance == null) {
+            instance = new Playfield();
+        }
+        return instance;
+    }
+}
